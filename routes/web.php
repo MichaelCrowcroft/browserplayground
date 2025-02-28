be7bdd69df461e214b842d22574f03090400138d
michaelcrowcroft <michaelcrowcroft@outlook.com>
2025-02-27 19:37:24 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,16 @@ Route::get('/home', function () {
 })->name('home');
 
 Route::get('/', [ListingController::class, 'index'])->name('listings.index');
+Route::get('/listings', function () {
+    return redirect('/');
+});
 Route::post('/listings', [ListingController::class, 'store'])->name('listings.store');
 Route::get('/listings/create', [ListingController::class, 'create'])->name('listings.create');
 Route::get('/listings/{listing}', [ListingController::class, 'show'])->name('listings.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
-        return redirect('/listings');
+        return redirect('/');
         // return Inertia::render('dashboard');
     })->name('dashboard');
 });
