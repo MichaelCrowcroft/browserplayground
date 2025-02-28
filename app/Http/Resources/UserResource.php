0aaa4fd617e6a523c7e50da7b4f159b83f46ba82
michaelcrowcroft <michaelcrowcroft@outlook.com>
2025-02-28 14:30:24 -0500

    .

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
@@ -12,6 +13,8 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'email' => $this->email,
+            'avatar' => Storage::get($this->avatar),
         ];
     }
 }
